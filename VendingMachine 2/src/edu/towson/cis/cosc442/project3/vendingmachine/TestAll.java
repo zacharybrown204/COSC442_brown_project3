b67b7c8933a651e3a4288070c3fb43c30ad9971c
Zachary Brown <zacharybrown204@yahoo.com>
2016-03-08 21:26:13 -0500

    Generated tests for vending machine 2
    Created html documentation of the coverage test on the first vending machine class.
    Generated the test for the second vending machine class. Most are broken. Fixed 2 or 3 so far.

---
+++ VendingMachine 2/src/edu/towson/cis/cosc442/project3/vendingmachine/TestAll.java
@@ -0,0 +1,34 @@
+package edu.towson.cis.cosc442.project3.vendingmachine;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * The class <code>TestAll</code> builds a suite that can be used to run all
+ * of the tests within its package as well as within any subpackages of its
+ * package.
+ *
+ * @generatedBy CodePro at 3/8/16 9:15 PM
+ * @author Zachary Brown
+ * @version $Revision: 1.0 $
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	VendingMachineTest.class,
+	VendingMachineItemTest.class,
+	VendingMachineExceptionTest.class,
+})
+public class TestAll {
+
+	/**
+	 * Launch the test.
+	 *
+	 * @param args the command line arguments
+	 *
+	 * @generatedBy CodePro at 3/8/16 9:15 PM
+	 */
+	public static void main(String[] args) {
+		JUnitCore.runClasses(new Class[] { TestAll.class });
+	}
+}
